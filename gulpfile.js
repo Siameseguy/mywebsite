ecec584a02a1caa83a3bf628ee0d8e6860aed48a
Phanousit Syhakhom <psyhakhom@gmail.com>
2019-05-15 14:45:54 -0400

    building for production

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,7 @@ function watch() {
     browserSync.init({
         // You can tell browserSync to use this directory and serve it as a mini-server
         server: {
-            baseDir: "/"
+            baseDir: "./"
         }
         // If you are already serving your website locally using something like apache
         // You can use the proxy setting to proxy that instead
